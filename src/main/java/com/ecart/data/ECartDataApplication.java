753729ca6a12a6e3a34a76913296a43b6a378a60
ratna1992 <ratnasrinivas1992@gmail.com>
2022-12-17 13:33:59 +0530

    initial commit

---
+++ src/main/java/com/ecart/data/ECartDataApplication.java
@@ -0,0 +1,13 @@
+package com.ecart.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECartDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECartDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecart/data/ECartDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecart.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECartDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
